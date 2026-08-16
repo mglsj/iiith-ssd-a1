5c8b25b15a284fbabad910dc7bd7f12757e6896c
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2026-08-16 18:39:03 +0530

    cache config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	cacheDir: "./cache/astro",
+	vite: {
+		cacheDir: "./cache/vite",
+		plugins: [tailwindcss()],
+	},
 });
